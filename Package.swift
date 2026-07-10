e3ae9f3aba3e559e77bb26e807ff6c279dc42e45
Oleksandr Trishyn <oleksandr.trishyn@8x8.com>
2026-07-10 14:30:06 -0700

    Add Voice SDK v0.3.4 release description.

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Wavecell",
-            url: "https://github.com/8x8/voice-sdk-releases-ios/releases/download/0.3.1/Wavecell.xcframework.zip",
-            checksum: "20fa0228fb38ed93337267757bbc04481b8bc4ca34ffa8673808839403b21784"
+            url: "https://github.com/8x8/voice-sdk-releases-ios/releases/download/0.3.4/Wavecell.xcframework.zip",
+            checksum: "47f31cee9448a174af7141a4888a75bc379aa96117856e3e35959306f463c62b"
         )
     ]
 )
